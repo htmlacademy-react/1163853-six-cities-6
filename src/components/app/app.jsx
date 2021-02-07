005614eb76b021c40372249487f4fee2644e08c2
Valdix <novaldix@gmail.com>
2021-02-07 22:32:44 +0300

    Подключает BrowserRouter, создает компонент ScreenLogin

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ScreenMain from '../screen-main/screen-main';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import {hotelSrtucture} from '../../utils/types';
+
+import ScreenMain from '../screen-main/screen-main';
+import ScreenLogin from '../screen-login/screen-login';
 
 const App = ({hotels}) => {
   return (
-    <ScreenMain
-      hotels={hotels}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <ScreenMain
+            hotels={hotels}
+          />
+        </Route>
+        <Route exact path="/login" component={ScreenLogin}/>
+        <Route>
+          <h2>WHAT THE HELL IS GOING ON HERE</h2>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
