07cb1e9ea856c5b9eafa2ba36be51097aaa189da
Valdix <novaldix@gmail.com>
2021-02-24 19:24:46 +0300

    Подключает createStore и Provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from './store/reducers/reducer';
 import {mockHotels} from './temp/hotel';
 
 import App from './components/app/app';
@@ -8,9 +11,17 @@ import {generateComments} from './temp/service';
 const hotels = mockHotels;
 const comments = generateComments(hotels.length);
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <App
-      hotels={hotels}
-      comments={comments}
-    />, document.querySelector(`#root`)
+    <Provider store={store}>
+      <App
+        hotels={hotels}
+        comments={comments}
+      />
+    </Provider>, document.querySelector(`#root`)
 );
+
+// createStore - создаёт хранилище
+// передаём в createStore - ссылку на функцию, назовём её reducer, которая будет обновлять хранилище
+// Provider - обёртка, которая соединяет redux с react
