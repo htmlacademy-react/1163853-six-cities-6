a051096b8b3febcb55029cbc8f47aff02870ee46
Valdix <novaldix@gmail.com>
2021-02-24 19:31:29 +0300

    Устанавливает redux-devtools-extension и передаёт в createStore функцию для для инспекции кода в браузере

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import {reducer} from './store/reducers/reducer';
 import {mockHotels} from './temp/hotel';
 
@@ -11,7 +12,7 @@ import {generateComments} from './temp/service';
 const hotels = mockHotels;
 const comments = generateComments(hotels.length);
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
     <Provider store={store}>
