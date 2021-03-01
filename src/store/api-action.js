7422bdc98a825730a229a674ec92aea6bf4624d8
Valdix <novaldix@gmail.com>
2021-03-01 22:12:26 +0300

    Добавляет в api-action асинхронные экшены checkAuth и login

--- src/store/api-action.js
+++ src/store/api-action.js
@@ -1,4 +1,5 @@
 import {adaptAllHotelsToClient} from '../services/adapter';
+import {AuthorizationStatus} from '../utils/constants';
 import {ActionCreator} from './action';
 
 export const fetchHotels = () => (dispatch, _getState, api) => (
@@ -7,3 +8,21 @@ export const fetchHotels = () => (dispatch, _getState, api) => (
     dispatch(ActionCreator.setHotels(hotels));
   })
 );
+
+export const checkAuth = () => (dispatch, _getState, api) => (
+  api.get(`/login`)
+    .then(() => dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
+    .catch(() => {})
+);
+
+export const login = ({login: email, password}) => (dispatch, _getState, api) => (
+  api.post(`/login`, {email, password})
+    .then(() => dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
+);
+
+// Как это работает на примере асинхронного экшена fetchHotels.
+//    Это функция, которая возвращает другую функцию, которой thunk докидывает параметры dispatch, _getState, api
+//    это позволит передать middlewate этот api
+//    Далее - axios.get возвращает json.response, далее - then и когда промис зарезолвится, делаем dispatch
+// checkAuth - необходима для проверки авторизован ли пользователь
+// login - необходим, чтобы залогиниться
